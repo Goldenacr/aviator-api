81171f3303492ba8e3313a75bd216cfe5261dbf3
Goldenacr <codmnovelah115+random20@gmail.com>
2026-06-24 21:37:27 +0000

    Update api-aviator.js

--- api-aviator.js
+++ api-aviator.js
@@ -6,7 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 const MONGO_URI = 'mongodb+srv://richvybs18:Fuckyou2026%24@cluster0.cq4ddne.mongodb.net/?appName=Cluster0';
 const DB_NAME = 'aviator';
